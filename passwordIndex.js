9fff7fb8cf2f4607723d5c83068c244ee8fb339b
Taylor Omondi Odhiambo <tayloromondi05@gmail.com>
2023-02-27 16:30:01 +0300

    to be continued
    vichwa tatu

--- passwordIndex.js
+++ passwordIndex.js
@@ -6,13 +6,28 @@ let key1 =""
 let key2 =""
 let randomIndex = Math.floor(Math.random()*store.length) 
 
-//size.addEventListener("input", generatePassword)
 
+//size.addEventListener("input", generatePassword)
+function getuppercase(){
+    return Math.floor(Math.random() * 26)//0 -25
+}
+function getlowercase(){
+    let nev = (Math.floor(Math.random() *52) + 26)
+    return nev
+    // return (Math.floor(Math.random() *52) + 26)//26-51
+}
+function getnumber(){
+    return (Math.floor(Math.random() * 62) + 36)//52-61
+}
+function getspecial(){
+    return (Math.floor(Math.random() *store.length) + 62)//62 - end of array
+
+}
 document.getElementById("generate-btn").addEventListener("click",function(){
     generatePassword()
 }
 )
-function generatePassword(){
+/*function generatePassword(){
     
     let size = document.getElementById("length")
     console.log(size.value)
@@ -31,4 +46,19 @@ function generatePassword(){
     document.getElementById("o1").textContent = key1
     document.getElementById("o2").textContent = key2
     //document.getElementById("length").value = 0
+}*/
+function generatePassword(){
+    let size = document.getElementById("length")
+    let x = 0
+    for (let i = 0; i < size.value/4; i++){
+        x = getuppercase()
+        key1 += store[x]
+        x = getlowercase()
+        key1 += store[x]
+        x = getnumber()
+        key1 += store[x]
+        x = getspecial()
+        key1 += store[x]
+    }
+    document.getElementById("o1").textContent = key1
 }
