32a03409c924838c5f6f58483d2657b0bc96103a
dom <kimeudom.git@gmail.com>
2023-02-27 21:00:43 +0300

    Fixed random text generation

--- passwordIndex.js
+++ passwordIndex.js
@@ -1,64 +1,56 @@
-let store = ["A", "B", "C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
+let store = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
 "U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r",
 "s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","!","@","#","$","%","&",
 "*","_","-"]
 let key1 =""
-let key2 =""
-let randomIndex = Math.floor(Math.random()*store.length) 
+let key2 = ""
 
+let lowerCase = {max:25, min:0}
+let upperCase = {max:51, min:26}
+let numberCase = {max:61, min:52}
+let specialCase = {max:62, min:(store.length - 1)}
 
-//size.addEventListener("input", generatePassword)
-function getuppercase(){
-    return Math.floor(Math.random() * 26)//0 -25
+function returnIndex(max, min) {
+    return Math.floor(Math.random() * (max - min)) + min
 }
-function getlowercase(){
-    let nev = (Math.floor(Math.random() *52) + 26)
-    return nev
-    // return (Math.floor(Math.random() *52) + 26)//26-51
-}
-function getnumber(){
-    return (Math.floor(Math.random() * 62) + 36)//52-61
-}
-function getspecial(){
-    return (Math.floor(Math.random() *store.length) + 62)//62 - end of array
 
-}
 document.getElementById("generate-btn").addEventListener("click",function(){
     generatePassword()
 }
+
 )
-/*function generatePassword(){
-    
-    let size = document.getElementById("length")
-    console.log(size.value)
-    key1 =""
-    key2 =""
-    for ( let i = 0; i < size.value; i++)
-    {
-        randomIndex = Math.floor(Math.random()*store.length) 
-        key1 += store[randomIndex]
+function generatePassword(){
+    // Reformat the strings on each function call
+    key1 = ""
+    key2 = ""
+
+    let size = document.getElementById("length").value
+    for (let i = 0; i < size; i++){
+        // 0 == lower case
+        // 1 == upper case
+        // 2 == number case
+        // 3 == special case
+        let chooseCase = Math.floor(Math.random() * 4)
+        switch (chooseCase) {
+            case 0:
+                key1 += store[returnIndex(lowerCase.max, lowerCase.min)]; 
+                key2 += store[returnIndex(lowerCase.max, lowerCase.min)]; 
+                break;
+            case 1:
+                key1 += store[returnIndex(upperCase.max, upperCase.min)]; 
+                key2 += store[returnIndex(upperCase.max, upperCase.min)]; 
+                break;
+            case 2:
+                key1 += store[returnIndex(numberCase.max, numberCase.min)]; 
+                key2 += store[returnIndex(numberCase.max, numberCase.min)]; 
+                break;
+            case 3:
+                key1 += store[returnIndex(specialCase.max, specialCase.min)]; 
+                key2 += store[returnIndex(specialCase.max, specialCase.min)]; 
+                break;
+        }
     }
-    for ( let i = 0; i < size.value; i++)
-    {
-        randomIndex = Math.floor(Math.random()*store.length) 
-        key2 += store[randomIndex]
-    }
+
     document.getElementById("o1").textContent = key1
     document.getElementById("o2").textContent = key2
-    //document.getElementById("length").value = 0
-}*/
-function generatePassword(){
-    let size = document.getElementById("length")
-    let x = 0
-    for (let i = 0; i < size.value/4; i++){
-        x = getuppercase()
-        key1 += store[x]
-        x = getlowercase()
-        key1 += store[x]
-        x = getnumber()
-        key1 += store[x]
-        x = getspecial()
-        key1 += store[x]
-    }
-    document.getElementById("o1").textContent = key1
 }
